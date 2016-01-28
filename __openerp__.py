34c927835bee983cfe98aa725d65391c6168d755
Vincent Hatakeyama <vincent.hatakeyama@xcg-consulting.fr>
2016-01-28 16:24:43 +0100

    7.0.2.0
    --HG--
    branch : 7.0

--- __openerp__.py
+++ __openerp__.py
@@ -30,7 +30,7 @@ templates.
 The py3o.template package is required; install it with:
     pip install py3o.template
 ''',
-    'version': '2.0a1',
+    'version': '7.0.2.0',
     'category': 'Reporting',
     'author': 'XCG Consulting',
     'website': 'http://odoo.consulting/',
